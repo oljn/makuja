036fe75b5d679c0a2f8b7b3b71b99a5f71192cff
Noora Oljakka <154086282+oljn@users.noreply.github.com>
2025-09-19 01:00:53 +0300

    Created database structure & added user registration

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import sqlite3
 from flask import Flask
+from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
+import db
 
 app = Flask(__name__)
 
@@ -6,3 +10,23 @@ app = Flask(__name__)
 def index():
     return "Tähän tulee ravintola-arvostelusovellus"
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "Virhe: syöttämäsi salasanat eivät ole samat."
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "Virhe: valitsemasi tunnus on jo käytössä."
+
+    return "Tunnus luotu."
